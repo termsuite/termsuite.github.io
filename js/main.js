5d952c5e7a566240824bbe3ea74fd508a4de40e0
Damien Cram <damien.cram@univ-nantes.fr>
2016-09-16 18:09:48 +0200

    Built from jekyll-site

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 
 $(document).ready(function(){
-  $("#personalemail").change(function() {
-    link='mailto:termsuite@univ-nantes.fr?subject=Subscription request: '
-          + $("#personalemail").val()
-          + '&body='
-          + $("#subscribe").data("body");
-    $("#subscribe").attr("href", encodeURI(link));
-  });
+  updateMailTo();
+  $("#personalemail").keyup(updateMailTo);
 });
+
+function updateMailTo() {
+  link='mailto:termsuite@univ-nantes.fr?subject=Subscription request: '
+        + $("#personalemail").val()
+        + '&body='
+        + $("#subscribe").data("body");
+  $("#subscribe").attr("href", encodeURI(link));
+}
