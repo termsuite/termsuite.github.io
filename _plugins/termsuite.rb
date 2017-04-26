2faecae8c05c3fab82f9c0e46332f5f640c3ad16
Damien Cram <damien.cram@univ-nantes.fr>
2017-04-26 10:54:12 +0200

    Added termsuite.rb utils for plugins

---
+++ _plugins/termsuite.rb
@@ -0,0 +1,19 @@
+class TermSuite
+  class << self
+    def config
+      @config ||= File.open("_termsuite.yml") { |file| YAML.load(file) }
+    end
+
+    def home
+      config["home"]
+    end
+
+    def version
+      Jekyll::Site.config["termsuite"]["version"]
+    end
+
+    def jar_path
+      File.join config["home"], "termsuite-core-#{version}.jar"
+    end
+  end
+end
